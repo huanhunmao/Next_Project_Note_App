c2fd48bdfa312375a103a45858ec21cf38096a2e
Marxu <fuhaojun@52tt.com>
2024-04-09 17:41:21 +0800

    feat: 造一些假数据 展示出 item 内容

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
 import { Fragment } from "react";
 import Hero from '../components/home-page/hero'
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+const  DUMMY_POSTS = [
+    {
+        slug:'getting-started-with-nextjs',
+        title:'Getting Started With NextJs',
+        image:'getting-started-nextjs.png',
+        excerpt:'NextJs is good fullstack app',
+        data:'2024-4-9',
+    },
+    {
+        slug:'getting-started-with-nextjs1',
+        title:'Getting Started With NextJs',
+        image:'getting-started-nextjs.png',
+        excerpt:'NextJs is good fullstack app',
+        data:'2024-4-9',
+    },
+    {
+        slug:'getting-started-with-nextjs2',
+        title:'Getting Started With NextJs',
+        image:'getting-started-nextjs.png',
+        excerpt:'NextJs is good fullstack app',
+        data:'2024-4-9',
+    },
+    {
+        slug:'getting-started-with-nextjs3',
+        title:'Getting Started With NextJs',
+        image:'getting-started-nextjs.png',
+        excerpt:'NextJs is good fullstack app',
+        data:'2024-4-9',
+    }
+]
 
 function HomePage(){
     return (
         <Fragment>
             <Hero/>
+            <FeaturedPosts posts={DUMMY_POSTS}/>
         </Fragment>
     )
 }
