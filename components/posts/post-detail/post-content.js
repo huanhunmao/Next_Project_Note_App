dc3e5fa2689016fbff9d1466ba61952e4a385f84
Marxu <fuhaojun@52tt.com>
2024-04-10 18:30:13 +0800

    feat: 动态加载 post page 详情页 & paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,22 +3,14 @@ import ReactMarkDown from 'react-markdown'
 import PostHeader from './post-header'
 import classes from './post-content.module.css'
 
-const  DUMMY_POSTS = 
-    {
-        slug:'getting-started-with-nextjs',
-        title:'Getting Started With NextJs',
-        image:'getting-started-nextjs.png',
-        content:'# It is content',
-        date:'2024-4-9',
-    }
-
-function PostContent(){
-    const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`
+function PostContent(props){
+    const { post } = props
+    const imagePath = `/images/posts/${post.slug}/${post.image}`
     return(
         <article className={classes.content}>
-        <PostHeader  image={imagePath} title={DUMMY_POSTS.title}/>
+        <PostHeader  image={imagePath} title={post.title}/>
         <ReactMarkDown>
-        {DUMMY_POSTS.content}
+        {post.content}
         </ReactMarkDown>
         </article>
     )
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,36 @@
 import PostContent from "../../components/posts/post-detail/post-content"
+import { getPostData, getPostFileNames } from "../../lib/posts-util"
 
 
-function PostDetailPage(){
-    return <PostContent/>
+function PostDetailPage(props){
+    return <PostContent post={props.post}/>
+}
+
+// 准备预渲染数据 
+export function getStaticProps(context){
+    const { params } = context
+    const { slug } = params
+
+    const postData = getPostData(slug+'.md')
+
+    return {
+        props:{
+            post: postData
+        },
+        revalidate: 600
+    }
+}
+
+// 准备预渲染的 paths 
+export function getStaticPaths(){
+    const postFileNames  = getPostFileNames()
+
+    const slugs = postFileNames.map(fileName => fileName.replace(/\.md$/, ''))
+
+    return {
+        paths: slugs.map(slug => ({params: {slug: slug}})),
+        fallback: false
+    }
 }
 
 export default PostDetailPage
