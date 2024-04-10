4dd8282d7ac3d5c18132d07334ed21a64aa5d672
Marxu <fuhaojun@52tt.com>
2024-04-10 20:38:47 +0800

    feat: 加载 markdown 图片

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,14 +2,29 @@
 import ReactMarkDown from 'react-markdown'
 import PostHeader from './post-header'
 import classes from './post-content.module.css'
+import Image from 'next/image'
 
 function PostContent(props){
     const { post } = props
     const imagePath = `/images/posts/${post.slug}/${post.image}`
+
+    const customRenders = {
+        img(image){
+            return (
+                <Image
+                src={`/images/posts/${post.slug}/${image.src}`}
+                alt={post.slug}
+                width={600}
+                height={300}
+                />
+            )
+        }
+    }
+
     return(
         <article className={classes.content}>
         <PostHeader  image={imagePath} title={post.title}/>
-        <ReactMarkDown>
+        <ReactMarkDown components={customRenders}>
         {post.content}
         </ReactMarkDown>
         </article>
