d981189372a2bdc57f7b509fcc9353f265a7d3c0
Marxu <fuhaojun@52tt.com>
2024-04-11 15:57:27 +0800

    feat: 优化图片展示

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -9,15 +9,36 @@ function PostContent(props){
     const imagePath = `/images/posts/${post.slug}/${post.image}`
 
     const customRenders = {
-        img(image){
-            return (
-                <Image
-                src={`/images/posts/${post.slug}/${image.src}`}
-                alt={post.slug}
-                width={600}
-                height={300}
+        // img(image){
+        //     return (
+                // <Image
+                // src={`/images/posts/${post.slug}/${image.src}`}
+                // alt={post.slug}
+                // width={600}
+                // height={300}
+                // />
+        //     )
+        // }
+
+        // 定义 p(paragraph) 渲染函数，可以优化 Markdown 中图片的展示。
+        // 它允许你根据 Markdown 内容中图片的位置和属性，动态地调整图片的展示方式和样式
+        p(paragraph){
+            const { node } = paragraph
+
+            if(node.children[0].tagName === 'img'){
+                const image = node.children[0]
+
+                return (
+                    <div className={classes.image}>
+                        <Image
+                        src={`/images/posts/${post.slug}/${image.properties.src}`}
+                        alt={post.alt}
+                        width={600}
+                        height={300}
                 />
-            )
+                    </div>
+                )
+            }
         }
     }
 
