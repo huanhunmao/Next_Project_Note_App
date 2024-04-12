0b9474e74c0ac48a8f606e5c0ee2da53c5e0442b
Marxu <fuhaojun@52tt.com>
2024-04-12 17:45:14 +0800

    feat: 使用 ReactDOM.createPortal 使得一部分 dom 脱离文档 独立开（使用 ReactDOM.createPortal() 可以将子组件渲染到 DOM 树中的任意位置，而不受父组件的 DOM 结构限制）

--- ui/notification.js
+++ ui/notification.js
@@ -21,12 +21,12 @@ function Notification(props) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal((
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
-  );
+  ),document.getElementById('notifications'));
 }
 
 export default Notification;
